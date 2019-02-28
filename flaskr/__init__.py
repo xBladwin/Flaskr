afcf0555176df8594a6b7566703681df11fc03e4
Jade Baldwin <wjbaldwin171@stevenscollege.edu>
2019-02-28 16:18:38 -0500

    second commit

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,12 +2,15 @@ import os
 
 from flask import Flask
 
+
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY = 'dev',
         DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent = True)
@@ -23,4 +26,7 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
+    from . import db
+    db.init_app(app)
+    
     return app
